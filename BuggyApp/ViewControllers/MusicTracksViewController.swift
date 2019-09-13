96dcf064b1cc5af1f11d8d506720dc4c190e96f4
ZGAME <58160323@go.buu.ac.th>
2019-09-13 18:53:47 +0700

    Show view item success

--- BuggyApp/ViewControllers/MusicTracksViewController.swift
+++ BuggyApp/ViewControllers/MusicTracksViewController.swift
@@ -71,7 +71,7 @@ extension MusicTracksViewController: UITableViewDelegate {
   
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     tableView.deselectRow(at: indexPath, animated: true)
-    self.performSegue(withIdentifier: "showDetail", sender: tracks)
+    self.performSegue(withIdentifier: "showDetail", sender: tracks[indexPath.row])
   }
 }
 
